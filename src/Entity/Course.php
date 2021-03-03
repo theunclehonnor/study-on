0a9086215699ca4b4764ee17ff0e9dc464659f0d
theunclehonnor <jekbormoley@gmail.com>
2021-03-03 13:10:25 +0300

    fix code unique

--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -5,10 +5,16 @@ namespace App\Entity;
 use App\Repository\CourseRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CourseRepository::class)
+ * @UniqueEntity(
+ *     fields={"code"},
+ *     message="This code is not unique."
+ * )
  */
 class Course
 {
@@ -20,7 +26,7 @@ class Course
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $code;
 
